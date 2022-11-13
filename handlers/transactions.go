d5ab158b328ea218a8385184b1134228f6d624c9
nishkarta <nishkarta@gmail.com>
2022-11-13 22:14:21 +0700

    test

--- handlers/transactions.go
+++ handlers/transactions.go
@@ -3,13 +3,24 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 	dto "waysbook/dto/results"
 	transactiondto "waysbook/dto/transactions"
 	"waysbook/models"
 	"waysbook/repositories"
 
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/coreapi"
+	"github.com/midtrans/midtrans-go/snap"
 )
+
+var c = coreapi.Client{
+	ServerKey: os.Getenv("SERVER_KEY"),
+	ClientKey: os.Getenv("CLIENT_KEY"),
+}
 
 type handlerTransaction struct {
 	TransactionRepository repositories.TransactionRepository
@@ -60,7 +71,18 @@ func (h *handlerTransaction) AddTransaction(w http.ResponseWriter, r *http.Reque
 		Status: "pending",
 	}
 
+	var TransIdIsMatch = false
+	var TransactionId int
+	for !TransIdIsMatch {
+		TransactionId = int(time.Now().Unix())
+		transactionData, _ := h.TransactionRepository.GetTransactionByID(TransactionId)
+		if transactionData.ID == 0 {
+			TransIdIsMatch = true
+		}
+	}
+
 	transaction := models.Transaction{
+		ID:      TransactionId,
 		BuyerID: request.BuyerID,
 		BookID:  request.BookID,
 		Total:   request.Total,
@@ -83,8 +105,80 @@ func (h *handlerTransaction) AddTransaction(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// 1. Initiate Snap client
+	var s = snap.Client{}
+	s.New(os.Getenv("SERVER_KEY"), midtrans.Sandbox)
+	// key := os.Getenv("SERVER_KEY")
+	// Use to midtrans.Production if you want Production Environment (accept real transaction).
+	// fmt.Println("ypour key server:", key)
+	// 2. Initiate Snap request param
+	req := &snap.Request{
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  strconv.Itoa(transaction.ID),
+			GrossAmt: int64(transaction.Total),
+		},
+		CreditCard: &snap.CreditCardDetails{
+			Secure: true,
+		},
+		CustomerDetail: &midtrans.CustomerDetails{
+			FName: transaction.Buyer.FullName,
+			Email: transaction.Buyer.Email,
+		},
+	}
+
+	// 3. Execute request create Snap transaction to Midtrans Snap API
+	snapResp, _ := s.CreateTransaction(req)
+
 	w.WriteHeader(http.StatusOK)
-	response := dto.SuccessResult{Code: "Success", Data: transaction}
+	response := dto.SuccessResult{Code: "Success", Data: snapResp}
 	json.NewEncoder(w).Encode(response)
 
+}
+
+func (h *handlerTransaction) Notification(w http.ResponseWriter, r *http.Request) {
+	var notificationPayload map[string]interface{}
+
+	err := json.NewDecoder(r.Body).Decode(&notificationPayload)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	transactionStatus := notificationPayload["transaction_status"].(string)
+	fraudStatus := notificationPayload["fraud_status"].(string)
+	orderId := notificationPayload["order_id"].(string)
+
+	// Get One Transaction from repository GetOneTransaction using orderId parameter here ...
+
+	if transactionStatus == "capture" {
+		if fraudStatus == "challenge" {
+			// TODO set transaction status on your database to 'challenge'
+			// e.g: 'Payment status challenged. Please take action on your Merchant Administration Portal
+			h.TransactionRepository.UpdateTransaction("pending", orderId)
+		} else if fraudStatus == "accept" {
+			// TODO set transaction status on your database to 'success'
+			// Call SendMail function here ...
+			h.TransactionRepository.UpdateTransaction("success", orderId)
+		}
+	} else if transactionStatus == "settlement" {
+		// TODO set transaction status on your databaase to 'success'
+		// Call SendMail function here ...
+		h.TransactionRepository.UpdateTransaction("success", orderId)
+	} else if transactionStatus == "deny" {
+		// TODO you can ignore 'deny', because most of the time it allows payment retries
+		// and later can become success
+		// Call SendMail function here ...
+		h.TransactionRepository.UpdateTransaction("failed", orderId)
+	} else if transactionStatus == "cancel" || transactionStatus == "expire" {
+		// TODO set transaction status on your databaase to 'failure'
+		// Call SendMail function here ...
+		h.TransactionRepository.UpdateTransaction("failed", orderId)
+	} else if transactionStatus == "pending" {
+		// TODO set transaction status on your databaase to 'pending' / waiting payment
+		h.TransactionRepository.UpdateTransaction("pending", orderId)
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
--- repositories/transaction.go
+++ repositories/transaction.go
@@ -10,6 +10,7 @@ type TransactionRepository interface {
 	FindTransactions() ([]models.Transaction, error)
 	GetCurrentCarts(userID int) ([]models.Cart, error)
 	CreateTransaction(transaction models.Transaction) (models.Transaction, error)
+	UpdateTransaction(status string, ID string) error
 	GetTransactionByID(ID int) (models.Transaction, error)
 }
 
@@ -41,4 +42,48 @@ func (r *repository) GetTransactionByID(ID int) (models.Transaction, error) {
 	err := r.db.First(&transaction, ID).Error
 
 	return transaction, err
+}
+
+// func (r *repository) UpdateTransaction(status string, ID string) error {
+// 	var transaction models.Transaction
+// 	r.db.First(&transaction, ID)
+
+// 	// new status : pending
+// 	// status : pending
+
+// 	// If is different & Status is "success" decrement product quantity
+// 	if status != transaction.Status && status == "success" {
+
+// 		var order []models.Order
+// 		orderedProduct, _ := r.GetTransactionProducts(order, transaction.ID)
+// 		for _, p := range orderedProduct {
+// 			var product models.Book
+// 			r.db.First(&product, p.ID)
+// 			r.db.Save(&product)
+// 		}
+// 	}
+
+// 	transaction.Status = status
+
+// 	err := r.db.Save(&transaction).Error
+
+// 	return err
+// }
+
+func (r *repository) UpdateTransaction(status string, ID string) error {
+	var transaction models.Transaction
+	r.db.Preload("Book").First(&transaction, ID)
+
+	// If is different & Status is "success" decrement product quantity
+	if status != transaction.Status && status == "success" {
+		var product models.Book
+		r.db.First(&product, transaction.Book)
+		r.db.Save(&product)
+	}
+
+	transaction.Status = status
+
+	err := r.db.Save(&transaction).Error
+
+	return err
 }
