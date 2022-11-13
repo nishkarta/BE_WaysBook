960bdbbc9cc2ef21ee9698367b753a1c3f598240
nishkarta <nishkarta@gmail.com>
2022-11-13 22:18:35 +0700

    test

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -2,10 +2,11 @@ package mysql
 
 import (
 	"fmt"
+	"os"
 	// "os"
 
-	// "gorm.io/driver/postgres"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	// "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -13,20 +14,20 @@ var DB *gorm.DB
 
 func DatabaseInit() {
 	var err error
-	dsn := "root:@tcp(127.0.0.1:3306)/waysbook?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "root:@tcp(127.0.0.1:3306)/waysbook?charset=utf8mb4&parseTime=True&loc=Local"
+	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
 	}
-	// var DB_HOST = os.Getenv("DB_HOST")
-	// var DB_USER = os.Getenv("DB_USER")
-	// var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-	// var DB_NAME = os.Getenv("DB_NAME")
-	// var DB_PORT = os.Getenv("DB_PORT")
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
 
-	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
-	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
